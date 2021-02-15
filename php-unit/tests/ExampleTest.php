bc07b5cd472e6850f65f8c072224e56f74f3fec4
nwkwok <nwkwok@gmail.com>
2021-02-15 11:22:06 -0800

    add notes

--- php-unit/tests/ExampleTest.php
+++ php-unit/tests/ExampleTest.php
@@ -20,13 +20,27 @@ class ExampleTest extends TestCase
     }
 }
 
-
 # Unit tests usually test the behavior of a function or class
 
 # To write a test in PHP-Unit
-    # Create a class that extends TestCase
-    # Add public methods that start with test
-    # Write public assertions that test code
+# Create a class that extends TestCase
+# Add public methods that start with test
+# Write public assertions that test code
 
 # There are many different assertion methods that are available which you can find here:
-    # https://phpunit.readthedocs.io/en/9.5/assertions.html
+# https://phpunit.readthedocs.io/en/9.5/assertions.html
+
+# Naming Test Methods:
+# There are two ways that you can name your test methods
+# Prefix the method name with 'test'
+// testFullNameIsEmptyByDefault
+// test_full_name_is_empty_by_default
+
+# Prefix your method with a docblock
+
+/** 
+ * @test
+ */
+
+// public function user_has_first_name()    
+// Because this is prefixed with the @test docblock, this test is now valid
