45511ae2168eb4c9f0c7181a2d908b5f10aaad18
nwkwok <nwkwok@gmail.com>
2021-03-25 21:55:49 -0700

    add fly trait to abstract class practice

--- oop/SOLIDPROFESSOR/abstract_classes/pet_example/classes/pet.php
+++ oop/SOLIDPROFESSOR/abstract_classes/pet_example/classes/pet.php
@@ -1,5 +1,15 @@
 <?php
 
+// Traits are both signature and implementation
+// Interfaces are signature and classes inform implementation
+// Abstract classes can hold both but if methods are prepended by abstract, then the classes must implement that method. 
+
+trait hasWings {
+    public function fly(){
+        return 'Wee! I can fly!';
+    }
+}
+
 abstract class Animal {
     private $fur;
     abstract public function setFur($bool);
@@ -82,6 +92,41 @@ class Cat extends Pet
     public function getType(){
         return $this->type;
     }
+}
+
+class Bird extends Pet
+{
+    use hasWings;
+        public function setFur($bool){
+            $this->fur = $bool;
+        }
+        public function getFur(){
+            return $this->fur;
+        }
+        public function speak()
+        {
+            return 'Chirp!';
+        }
+
+        public function sleep()
+        {
+            return 'This perch looks comfy...';
+        }
+        public function hasFur(){
+            $this->fur = true;
+        }
+        public function setName($name){
+            $this->name = $name;
+        }
+        public function getName(){
+            return $this->name;
+        }
+        public function setType($type){
+            $this->type = $type;
+        }
+        public function getType(){
+            return $this->type;
+        }
 }
 
 
