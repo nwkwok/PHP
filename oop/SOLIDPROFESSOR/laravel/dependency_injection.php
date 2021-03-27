b5f96f2cc072f58323c3414128f18664c5030386
nwkwok <nwkwok@gmail.com>
2021-03-26 23:27:33 -0700

    change dependency injection notes to php for better code readability

--- oop/SOLIDPROFESSOR/laravel/dependency_injection.php
+++ oop/SOLIDPROFESSOR/laravel/dependency_injection.php
@@ -1,9 +1,10 @@
-## What is Dependency Injection?
+<?php
+/*
+#################What is Dependency Injection?#########################
 Dependency injection is a fancy phrase that essentially means: class dependencies are 'injected' into the class via the the constructor or a 'setter' method. 
 
 Here is an example: 
-
-<?php
+*/
 
 namespace App;
 
@@ -22,16 +23,15 @@ class Client {
     }
 }
 
+/*
 In the contructor, you can see the $service property will be the value of a new instantiation from the UserService class and $service will now have access to all of the properties and methods of the UserService class. 
 
 This means that class Client now has a dependency on class UserService because if class UserService changes in a substantial way, it would affect class Client.
 
-### Constructor Dependency Injection
+
+#######################Constructor Dependency Injection########################
 Taking the example from above:
-
-<?php
-
-namespace App;
+*/
 
 class Client {
     // Internal reference to the service used by this client
@@ -48,18 +48,10 @@ class Client {
     }
 }
 
-Here we are 'injecting' a dependency explicitly into the class by type-hinting the UserService into the constructor.
+# Here we are 'injecting' a dependency explicitly into the class by type-hinting the UserService into the constructor.
 
-### How does this work in Laravel?
-Lets take a look at an example controller: 
+##################### How does this work in Laravel? ######################
 
-<?php
-
-namespace AppHttpControllers;
-
-use AppUser;
-use AppRepositoriesUserRepository;
-use AppHttpControllersController;
 
 class UserController extends Controller
 {
@@ -95,6 +87,7 @@ class UserController extends Controller
     }
 }
 
+/*
 In this example, the UserController needs to retrieve users from a data source. So, we will inject a service that is able to retrieve users. In this context, our UserRepository most likely uses Eloquent to retrieve user information from the database. However, since the repository is injected, we are able to easily swap it out with another implementation. We’re also able to easily “mock”, or create a dummy implementation of the UserRepository when testing our application.
 
 The benefit of constructor dependency injection is that we get to choose the dependency that gets passed into the constructor. If we need a different implementation, that's no problem because we can just pass that into the constructor. 
@@ -103,12 +96,17 @@ What Laravel does in above example is it will check what dependency has been pas
 
 The above method resolves automatically. But there’s an alternative way of doing this in Laravel. i.e using service container bindings. So, we can bind UserRepository class into the UserController class like this into the service provider.
 
-### Method Dependency Injection
+######################## Method Dependency Injection #########################
 There will be a case when you only want to inject the depdency into a certain method. In such scenarios, you can use method dependency injection whereby you inject the object to your class through a setter method instead of a constructor. 
 
-public function getSettings(UserSettings $settings)
+*/ 
+
+class UserController extends Controller
 {
-    return $settings->get();
+    public function getSettings(UserSettings $settings)
+        {
+            return $settings->get();
+        }
 }
 
-As you can see, in this case if we know that the method getSettings is the only method that is going to use UserSettings, we wouldn't need to inject it. So, instead we’ve injected it right into the method itself which Laravel supports out-of-the-box.
+# As you can see, in this case if we know that the method getSettings is the only method that is going to use UserSettings, we wouldn't need to inject it. So, instead we’ve injected it right into the method itself which Laravel supports out-of-the-box.
